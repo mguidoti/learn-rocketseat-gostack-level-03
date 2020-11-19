b7ac90bc3629597e58d13be643c9ee006bfed265
mguidoti <marcus.guidoti@gmail.com>
2020-11-19 17:31:49 -0300

    feat: Code for "Conectando a API" class

--- primeiro-projeto-react/src/pages/Dashboard/index.tsx
+++ primeiro-projeto-react/src/pages/Dashboard/index.tsx
@@ -1,57 +1,71 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 
 import { FiChevronRight } from 'react-icons/fi';
 
 import logoImg from '../../assets/logo.svg';
 
 import { Title, Form, Repositories } from './styles';
+
+import api from '../../services/Api';
 // Same as function Dashboard() {}, but with the second way, I can more easily
 // define type
+
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
 const Dashboard: React.FunctionComponent = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  async function handleAddRepository(
+    event: FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    // This prevents the default behavior of reloading the page after clicking
+    // the form button
+    event.preventDefault();
+
+    const response = await api.get<Repository>(`/repos/${newRepo}`);
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
+    setNewRepo('');
+  }
+
   return (
     <>
       <img src={logoImg} alt="Github Explorer" />
       <Title>Explore Repositórios no GitHub</Title>
 
-      <Form>
-        <input placeholder="Digite o nome do repositório" />
+      <Form onSubmit={handleAddRepository}>
+        <input
+          value={newRepo}
+          onChange={e => setNewRepo(e.target.value)}
+          placeholder="Digite o nome do repositório"
+        />
         <button type="submit">Pesquisar</button>
       </Form>
 
       <Repositories>
-        <a href="teste">
-          <img
-            src="https://avatars3.githubusercontent.com/u/44419136?s=460&v=4"
-            alt="Marcus Guidoti"
-          />
-          <div>
-            <strong>mguidoti/lycophron</strong>
-            <p>description of the repo</p>
-          </div>
-          <FiChevronRight />
-        </a>
-        <a href="teste">
-          <img
-            src="https://avatars3.githubusercontent.com/u/44419136?s=460&v=4"
-            alt="Marcus Guidoti"
-          />
-          <div>
-            <strong>mguidoti/lycophron</strong>
-            <p>description of the repo</p>
-          </div>
-          <FiChevronRight />
-        </a>
-        <a href="teste">
-          <img
-            src="https://avatars3.githubusercontent.com/u/44419136?s=460&v=4"
-            alt="Marcus Guidoti"
-          />
-          <div>
-            <strong>mguidoti/lycophron</strong>
-            <p>description of the repo</p>
-          </div>
-          <FiChevronRight />
-        </a>
+        {repositories.map(repository => (
+          <a key={repository.full_name} href="teste">
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+            <FiChevronRight />
+          </a>
+        ))}
       </Repositories>
     </>
   );
