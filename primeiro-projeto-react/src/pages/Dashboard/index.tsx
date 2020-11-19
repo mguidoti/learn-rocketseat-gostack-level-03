a4a14fc039cc6ab3d608a13d1324bbe8774865a6
mguidoti <marcus.guidoti@gmail.com>
2020-11-19 09:18:12 -0300

    feat: Code for "Criando Rotas" class

---
+++ primeiro-projeto-react/src/pages/Dashboard/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// Same as function Dashboard() {}, but with the second way, I can more easily
+// define type
+const Dashboard: React.FunctionComponent = () => {
+  return <h1>Dashboard</h1>;
+};
+
+export default Dashboard;
---
+++ primeiro-projeto-react/src/pages/Repository/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// Same as function Dashboard() {}, but with the second way, I can more easily
+// define type
+const Repository: React.FunctionComponent = () => {
+  return <h1>Repository</h1>;
+};
+
+export default Repository;
