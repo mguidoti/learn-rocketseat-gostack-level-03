3cbb3dceeee6b60a9b8c9d79f68ac1bd2eac165e
mguidoti <marcus.guidoti@gmail.com>
2020-11-19 17:02:44 -0300

    feat: Code for "Estilizando Dashboard" class

--- primeiro-projeto-react/src/pages/Dashboard/index.tsx
+++ primeiro-projeto-react/src/pages/Dashboard/index.tsx
@@ -1,10 +1,60 @@
 import React from 'react';
 
-import { Title } from './styles';
+import { FiChevronRight } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 // Same as function Dashboard() {}, but with the second way, I can more easily
 // define type
 const Dashboard: React.FunctionComponent = () => {
-  return <Title>Explore Repositórios no GitHub</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore Repositórios no GitHub</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars3.githubusercontent.com/u/44419136?s=460&v=4"
+            alt="Marcus Guidoti"
+          />
+          <div>
+            <strong>mguidoti/lycophron</strong>
+            <p>description of the repo</p>
+          </div>
+          <FiChevronRight />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars3.githubusercontent.com/u/44419136?s=460&v=4"
+            alt="Marcus Guidoti"
+          />
+          <div>
+            <strong>mguidoti/lycophron</strong>
+            <p>description of the repo</p>
+          </div>
+          <FiChevronRight />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars3.githubusercontent.com/u/44419136?s=460&v=4"
+            alt="Marcus Guidoti"
+          />
+          <div>
+            <strong>mguidoti/lycophron</strong>
+            <p>description of the repo</p>
+          </div>
+          <FiChevronRight />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
